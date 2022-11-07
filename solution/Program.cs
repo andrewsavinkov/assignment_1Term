8177e91ec6797defe5fd1cf41f2c27a2779b1be3
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-11-07 18:57:25 +0500

    написал программу

---
+++ solution/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] inputArray = { "Russia", "Denmark", "Kazan"};
+int lengthToShow = 3;
+
+void ArrayPrintOut(string[] input)
+{
+    Console.WriteLine("[" + string.Join(", ", input) + "]");
+}
+
+string[] ArraySorter(string[] input, int lengthToShow)
+{
+    List<string> sortedOut = new List<string>();
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length<=lengthToShow)
+            sortedOut.Add(input[i]);
+    }
+    string[] sortedArray = new string[sortedOut.Count];
+    for (int i = 0; i < sortedOut.Count; i++)
+        sortedArray[i]=sortedOut[i];
+    return sortedArray;
+}
+
+Console.WriteLine("unsorted: ");
+ArrayPrintOut(inputArray);
+string[] srtArray = ArraySorter(inputArray, lengthToShow);
+Console.WriteLine("sorted: ");
+ArrayPrintOut(srtArray);
